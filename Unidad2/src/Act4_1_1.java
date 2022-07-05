db17dd0f0858c9dcb8bc1dcebb611c6561296c51
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-05 16:09:58 +0200

    Unidad 2 1

---
+++ Unidad2/src/Act4_1_1.java
@@ -0,0 +1,28 @@
+public class Act4_1_1 {
+
+    public static void main (String args []) {
+           
+           // Valores
+
+           int a = 19, b = 91;
+
+           // Operaciones numéricas
+
+           int suma = a + b;
+
+           int resta = a-b;
+           
+           int division = a/b;
+
+           int multiplicacion = a*b;
+
+           System.out.println("El valor de la suma es: " + suma);
+           
+           System.out.print("El valor de la suma es: " + resta);
+           
+           System.out.print("El valor de la suma es: " + division);
+           
+           System.out.print("El valor de la suma es: " + multiplicacion);
+           
+        }
+}
